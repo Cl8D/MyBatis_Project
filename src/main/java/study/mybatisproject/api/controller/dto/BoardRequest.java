e1b108a0e744824b2ea2cf0181877aecf3473c54
jiwon <ljwon77@daum.net>
2022-07-29 02:23:45 +0900

    Feat: #10 - BoardRequest 생성자 추가

--- src/main/java/study/mybatisproject/api/controller/dto/BoardRequest.java
+++ src/main/java/study/mybatisproject/api/controller/dto/BoardRequest.java
@@ -1,11 +1,18 @@
 package study.mybatisproject.api.controller.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class BoardRequest {
     private int boardId;
     private String title;
     private String content;
+
+    public BoardRequest(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
 
