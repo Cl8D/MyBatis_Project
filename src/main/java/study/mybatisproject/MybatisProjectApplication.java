9616f6ef2295f02476ec5358d6e72f2371a8dd79
jiwon <ljwon77@daum.net>
2022-07-30 18:11:31 +0900

    Feat: #19 - 스케줄러 설정 추가

--- src/main/java/study/mybatisproject/MybatisProjectApplication.java
+++ src/main/java/study/mybatisproject/MybatisProjectApplication.java
@@ -2,8 +2,10 @@ package study.mybatisproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // 스케줄러 추가
 public class MybatisProjectApplication {
 
 	public static void main(String[] args) {
