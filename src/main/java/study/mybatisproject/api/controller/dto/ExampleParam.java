d8d8787bc78a90caf1ac17e9b8e7c380da0d9787
jiwon <ljwon77@daum.net>
2022-07-31 18:45:29 +0900

    #25 - 사용자 정보 DTO 추가

---
+++ src/main/java/study/mybatisproject/api/controller/dto/ExampleParam.java
@@ -0,0 +1,9 @@
+package study.mybatisproject.api.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class ExampleParam {
+    private String id;
+    private String code;
+}
---
+++ src/main/java/study/mybatisproject/api/controller/dto/ExampleUserRequest.java
@@ -0,0 +1,8 @@
+package study.mybatisproject.api.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class ExampleUserRequest {
+    private ExampleUser user;
+}
